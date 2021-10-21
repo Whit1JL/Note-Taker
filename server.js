137b156b399f1b15fa97fbf1d4045b14b87a0b94
Jessica White <jessica2714white@gmail.com>
2021-10-21 11:30:08 -0500

    Created server.js

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express")
+const fs = require("fs")
+const path = require("path")
+const favicon = require('express-favicon')
+
+
+//create an express server and port
+const app = express()
+const PORT = process.env.PORT || 3000
+
+
+// Set up the Express app to handle data parsing
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+app.use(express.static("public"))
+app.use(favicon(__dirname + '/public/favicon.png'))
+
+//linking Routes
+require("./routing/api-route")(app)
+require("./routing/html-route")(app)
+
+
+//server listener
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT)
+})
+ // favicon listener
+const server = app.listen(2000, function(){
+    console.log('server is running at %s .', server.address().port);
+});
