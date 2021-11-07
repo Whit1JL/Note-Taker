027877411ceb1ab8f42e7ecc215c4a5a17d1d763
Jessica White <jessica2714white@gmail.com>
2021-11-07 11:31:02 -0600

    Fixed error

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 // retrieve notes.html file
 router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../assests/notes.html'));
+    res.sendFile(path.join(__dirname, '../assets/notes.html'));
 });
 
 // get other routes to touch index.html 
