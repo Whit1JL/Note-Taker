2c2d32e5dafcf154f6ee34fb45c58c249a84ecab
Jessica White <jessica2714white@gmail.com>
2021-11-07 11:30:40 -0600

    Send routes to index html file

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,5 +3,12 @@ const path = require('path');
 
 // retrieve notes.html file
 router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/notes.html'));
-})
+    res.sendFile(path.join(__dirname, '../assests/notes.html'));
+});
+
+// get other routes to touch index.html 
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../assets/index.html'));
+});
+
+module.exports = router;
