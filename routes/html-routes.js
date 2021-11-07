5943549a05d63b9873db2677548a0a2af7895edd
Jessica White <jessica2714white@gmail.com>
2021-11-07 11:27:31 -0600

    Retrieve notes html file

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,2 +1,7 @@
 const router = require('express').Router();
 const path = require('path');
+
+// retrieve notes.html file
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+})
