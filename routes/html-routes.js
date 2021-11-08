04c61f0ba2c6b7c9a02a0f85c558c2f0a7a57797
Jessica White <jessica2714white@gmail.com>
2021-11-08 05:12:02 -0600

    Changed path and removed html export

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,16 +1,14 @@
 const path = require('path');
 
 module.exports = function(app) {
-    
+
 }
 // retrieve notes.html file
 router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/assets/notes.html'));
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
 
 // get other routes to touch index.html 
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/assets/index.html'));
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 });
-
-module.exports = html;
