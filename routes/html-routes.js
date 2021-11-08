584ac4c0e726a28181256a6936411db0657bacb6
Jessica White <jessica2714white@gmail.com>
2021-11-08 05:07:58 -0600

    Changed router to html

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,14 +1,15 @@
-const router = require('express').Router();
+const html = require('express').Router();
 const path = require('path');
+const router = require('express').Router();
 
 // retrieve notes.html file
 router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../assets/notes.html'));
+    res.sendFile(path.join(__dirname, '../public/assets/notes.html'));
 });
 
 // get other routes to touch index.html 
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../assets/index.html'));
+    res.sendFile(path.join(__dirname, '../public/assets/index.html'));
 });
 
-module.exports = router;
+module.exports = html;
