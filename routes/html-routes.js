f4b231b5c9f420a6ac67a390a3482707c75b50d1
Jessica White <jessica2714white@gmail.com>
2021-11-11 22:45:29 -0600

    Moved code around to make it display

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,17 +1,17 @@
 const path = require('path');
 const router = require('express').Router();
 
-// retrieve home page
-router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-});
-
 // retrieve notes.html file
 router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
 
-// get other routes to touch index.html 
+// retrieve home page
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+// get other routes to touch index.html and if no matching route is found this is default
 router.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
