bfab4bb336d57564266ff31b4fc7c4b15be33e5b
Jessica White <jessica2714white@gmail.com>
2021-11-11 22:45:08 -0600

    Utilize saveData db to get website to respond

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,45 +1,35 @@
 const router = require('express').Router();
-var noteData = require('../db/db.json');
-const { v4: uuidv4 } = require('uuid');
-const fs = require('fs');
+var saveData = require('../db/saveData');
+// const { v4: uuidv4 } = require('uuid');
+// const fs = require('fs');
 
 
 // API ROUTE
     //get request 
-    router.get("/notes", function (req,res){
-        res.json(JSON.parse(fs.readFileSync("./db/db.json", "utf8")))
-    })
+    router.get("/notes", function (req,res) {
+        saveData
+            .retrieveNotes()
+            .then(notes => res.json(notes))
+            .catch(err => res.status(500).json(err));
+        // res.json(JSON.parse(fs.readFileSync("./db/db.json", "utf8")))
+    });
 
     //post request
-    router.post("/notes", function (req,res){
-        req.body.id = uuidv4()
-        noteData.push(req.body)
-        fs.writeFile("../db/db.json", JSON.stringify(noteData), function(err){
-            if(err) {
-                throw error
-            }
-            res.json("saved")
-        })
+    router.post("/notes", function (req,res) {
+        saveData
+            .addNote(req.body)
+            .then((note) => res.json(note))
+            .catch(err => res.status(500).json(err));
         
-    })
+    });
+
     //delete request
-    router.delete("/notes/:id", function (req,res){
-        var note = req.params.id
-        console.log(note)
-        var journal = []
-  
-        for (var i = 0; i < noteData.length; i++) {
-          if (noteData[i].id !== note) {
-             journal.push(noteData[i]);
-          }
-        }
-        noteData = journal
-        fs.writeFile("./db/db.json", JSON.stringify(noteData), function(err){
-            if(err) {
-                throw error
-            }
-            res.json("deleted")
-        })
+    router.delete("/notes/:id", function (req,res) {
+        saveData
+            .deleteNote(req.params.id)
+            .then(() => res.json({ ok: true}))
+            .catch(err => res.status(500).json(err));
    
-    })
+    });
+    
 module.exports = router;
